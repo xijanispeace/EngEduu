9fd864dda7cded7222001347e9cc8b52a4f4330b
xijanispeace <xijanispeace@DESKTOP-8T29PJ0>
2019-10-04 20:36:59 +0700

    add “first year semester 1”

---
+++ EngEdu/EngEdu/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngEdu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year 255x semtember1,I am an Engedu freshman.");
+        }
+    }
+}

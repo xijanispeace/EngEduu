6113e24634c160f3d6b5388689e4100c2965ae49
xijanispeace <xijanispeace@DESKTOP-8T29PJ0>
2019-10-04 20:41:29 +0700

    add “first year semester 2”

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -11,6 +11,7 @@ namespace EngEdu
         static void Main(string[] args)
         {
             Console.WriteLine("Year 255x semtember1,I am an Engedu freshman.");
+            Console.WriteLine("Year 255x semtember2,I learn to program in C sharp");
         }
     }
 }
